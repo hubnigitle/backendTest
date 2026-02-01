84aa189e2946310a41f81a87ce324c69a0aff863
hubnigitle <maulenirh@gmail.com>
2026-02-01 09:45:28 +0700

    freat: init structure

---
+++ server.js
@@ -0,0 +1,16 @@
+var x = "hello"
+var x = 1
+//bisa di redelcare
+console.log(x)
+
+let y = "hello"
+y = 1
+//tidak bisa di redeclare
+//bisa diuba valuenya aja
+
+console.log(y)
+
+const z = "hello"
+//tidak bisa di redeclare
+//tidak bisa di ubah valuenya
+console.log(z)
